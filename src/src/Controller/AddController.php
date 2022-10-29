ce3b9b60a52a6a5fed779805d8e979eb6209de8b
Raphael Truong <r_truong1@hetic.eu>
2022-10-29 18:03:46 +0200

    fix project2

--- src/src/Controller/AddController.php
+++ src/src/Controller/AddController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Form\PostFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AddController extends AbstractController
 {
     #[Route('/add', name: 'app_add')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, ): Response
     {
+        $annonce = new Article();
+        $user = $this->getUser();
+        $form = $this->createForm(PostFormType::class, $annonce);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $annonce -> setIdSeller($user)
+                ->setIsAvailable(true)
+                ->setCreatedAt(new \DateTimeImmutable());
+            //dd($annonce);
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_main');
+        }
+
         return $this->render('add/index.html.twig', [
-            'controller_name' => 'AddController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/src/Form/PostFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('price', MoneyType::class)
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class
+            ])
+            ->add('Ajouter', SubmitType::class)
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
